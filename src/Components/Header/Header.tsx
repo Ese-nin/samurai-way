2ad804c078676250be2bbb1bd6324018282baf8a
Сергей <privet15.89@mail.ru>
2022-08-13 13:41:53 +0500

    add "alt" into tags <img/>

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,7 +4,7 @@ import "./Header.css"
 const Header = () => {
     return <div className={'header'}>
         <img
-            src={'https://playapkmod.com/files/2021/10/Download-Logo-Maker-Create-Logos-and-Icon-Design-Creator.png'}/>
+            src={'https://playapkmod.com/files/2021/10/Download-Logo-Maker-Create-Logos-and-Icon-Design-Creator.png'} alt={''}/>
     </div>
 }
 
