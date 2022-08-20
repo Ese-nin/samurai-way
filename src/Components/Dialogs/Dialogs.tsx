aa08d24692c6aa2ba77f90b3a7e78059b06db04d
Сергей <privet15.89@mail.ru>
2022-08-20 23:46:29 +0500

    dialogs вёрстка

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -3,8 +3,36 @@ import s from "./Dialogs.module.css"
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItem}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Leonid
+                </div>
+                <div className={s.dialog}>
+                    Victoria
+                </div>
+                <div className={s.dialog}>
+                    Tamara
+                </div>
+                <div className={s.dialog}>
+                    Stepan
+                </div>
+                <div className={s.dialog}>
+                    George
+                </div>
+                <div className={s.dialog}>
+                    Kate
+                </div>
+                <div className={s.dialog}>
+                    Maria
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi, how are you?</div>
+                <div className={s.message}>I'm busy</div>
+                <div className={s.message}>Where you from?</div>
+                <div className={s.message}>A-a-a-a-a-a</div>
+            </div>
         </div>
     )
 }
