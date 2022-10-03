07e79eecc38f471c3097ff7b60653b4fd7d71e57
Сергей <privet15.89@mail.ru>
2022-10-03 13:52:53 +0500

    refactoring

--- src/Redux/Reducers/dialogs-reducer.tsx
+++ src/Redux/Reducers/dialogs-reducer.tsx
@@ -27,44 +27,48 @@ const ADD_MESSAGE = "ADD-MESSAGE";
 export const addMessageAC = (messageText: string) => {
     return {
         type: ADD_MESSAGE,
-        newMessageText: messageText,
+        payLoad: {
+            newMessageText: messageText
+        },
     } as const
 }
 export const textareaMessageChangeAC = (messageText: string) => {
     return {
         type: UPDATE_MESSAGE_TEXT,
-        newMessageText: messageText,
+        payLoad: {
+            newMessageText: messageText
+        },
     } as const
 }
 
 const initialState = {
-        dialogs: [
-            {id: v1(), name: "Leonid"},
-            {id: v1(), name: "Victoria"},
-            {id: v1(), name: "Tamara"},
-            {id: v1(), name: "Stepan"},
-            {id: v1(), name: "George"},
-            {id: v1(), name: "Kate"},
-            {id: v1(), name: "Maria"}
-        ],
-        messages: [
-            {id: v1(), message: "Hi, how are you?"},
-            {id: v1(), message: "I'm busy"},
-            {id: v1(), message: "Where you from?"},
-            {id: v1(), message: "A-a-a-a-a-a"}
-        ],
-        newMessageText: "it-incubator",
-    };
+    dialogs: [
+        {id: v1(), name: "Leonid"},
+        {id: v1(), name: "Victoria"},
+        {id: v1(), name: "Tamara"},
+        {id: v1(), name: "Stepan"},
+        {id: v1(), name: "George"},
+        {id: v1(), name: "Kate"},
+        {id: v1(), name: "Maria"}
+    ],
+    messages: [
+        {id: v1(), message: "Hi, how are you?"},
+        {id: v1(), message: "I'm busy"},
+        {id: v1(), message: "Where you from?"},
+        {id: v1(), message: "A-a-a-a-a-a"}
+    ],
+    newMessageText: "it-incubator",
+};
 
 const dialogsReducer = (state: messagesPageType = initialState, action: ActionTypes): messagesPageType => {
 
     switch (action.type) {
         case UPDATE_MESSAGE_TEXT:
-            return {...state, newMessageText: action.newMessageText};
+            return {...state, newMessageText: action.payLoad.newMessageText};
         case ADD_MESSAGE:
             const newMessage = {
                 id: v1(),
-                message: action.newMessageText,
+                message: action.payLoad.newMessageText,
             };
             return {...state, messages: [...state.messages, newMessage], newMessageText: ""};
         default:
--- src/Redux/Reducers/profile-reducer.tsx
+++ src/Redux/Reducers/profile-reducer.tsx
@@ -21,24 +21,28 @@ const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 export const textareaChangeAC = (postText: string) => {
     return {
         type: UPDATE_NEW_POST_TEXT,
-        newText: postText,
+        payLoad: {
+            newText: postText
+        },
     } as const
 }
 export const addPostAC = (postText: string) => {
     return {
         type: ADD_POST,
-        newPostText: postText,
+        payLoad: {
+            newPostText: postText
+        },
     } as const
 }
 
 const initialState = {
-        posts: [
-            {id: v1(), message: "Hallo, mein freund", likesCount: 3},
-            {id: v1(), message: "It's my first post", likesCount: 45},
-            {id: v1(), message: "Good bye", likesCount: 1792}
-        ],
-        newPostText: "IT-Kamasutra"
-    };
+    posts: [
+        {id: v1(), message: "Hallo, mein freund", likesCount: 3},
+        {id: v1(), message: "It's my first post", likesCount: 45},
+        {id: v1(), message: "Good bye", likesCount: 1792}
+    ],
+    newPostText: "IT-Kamasutra"
+};
 
 const profileReducer = (state: profilePageType = initialState, action: ActionTypes): profilePageType => {
 
@@ -46,12 +50,12 @@ const profileReducer = (state: profilePageType = initialState, action: ActionTyp
         case ADD_POST:
             const newPost = {
                 id: v1(),
-                message: action.newPostText,
+                message: action.payLoad.newPostText,
                 likesCount: 0
             };
             return {...state, posts: [...state.posts, newPost], newPostText: ""};
         case UPDATE_NEW_POST_TEXT:
-            return {...state, newPostText: action.newText};
+            return {...state, newPostText: action.payLoad.newText};
         default:
             return state;
     }
