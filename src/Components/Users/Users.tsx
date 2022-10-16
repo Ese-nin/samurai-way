709b515fce1db6765a8c0e6097ef11b523a322ff
Сергей <privet15.89@mail.ru>
2022-10-17 02:22:44 +0500

    pagination

--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -11,17 +11,43 @@ export class Users extends React.Component<MapDispatchPropsType, MapStatePropsTy
     }
 
     componentDidMount() {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    this.props.setUsers(response.data.items);
-                });
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount)
+            });
+    }
+
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
     }
 
     render() {
+
+        const pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
         return (
             <div>
+                <div>
+                    {pages.map(el => {
+                        return <span key={el}
+                                     className={el === this.props.currentPage ? s.selectedPage : ''}
+                                     onClick={() => this.onPageChanged(el)}>
+                            {el}</span>
+                    })}
+                </div>
                 {this.props.users.map(el =>
                     <div key={el.id}>
+
                 <span>
                     <div>
                         <img className={s.userPhoto} src={el.photos.small !== null ? el.photos.small : userPhoto}
@@ -34,7 +60,8 @@ export class Users extends React.Component<MapDispatchPropsType, MapStatePropsTy
                         }
                     </div>
                 </span>
-                        <span>
+
+                <span>
                     <span>
                         <div>{el.name}</div>
                         <div>{el.status}</div>
