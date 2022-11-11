be30d96f96f573c9a7fe36f06302b142aacd16ba
Сергей <privet15.89@mail.ru>
2022-11-11 21:59:15 +0500

    used an actual "render" method

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import './index.css';
 import {store} from "./Redux/redux-store";
 import React from "react";
-import ReactDOM from 'react-dom';
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
+import {createRoot} from 'react-dom/client';
 
 
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>,
-        document.getElementById('root')
-    );
+const root = createRoot(document.getElementById('root')!); // createRoot(container!) if you use TypeScript
+root.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>
+);
