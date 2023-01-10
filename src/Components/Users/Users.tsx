cdf7ec8df4718d67742d017c941c55ed77ecb934
Сергей <privet15.89@mail.ru>
2023-01-10 14:54:08 +0500

    pagination

--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -4,41 +4,42 @@ import {Pagination} from "../common/Pagination/Pagination";
 import {User} from "./User";
 
 type UsersPropsType = {
-    totalUsersCount: number
-    pageSize: number
-    currentPage: number
-    onPageChanged: (currentPage: number) => void
-    users: Array<UsersType>
-    unfollow: (userId: string) => void
-    follow: (userId: string) => void
-    toggleIsFollowing: (isFetching: boolean, userID: string) => void
-    followingInProgress: string[]
+  totalUsersCount: number
+  pageSize: number
+  currentPage: number
+  onPageChanged: (currentPage: number) => void
+  users: Array<UsersType>
+  unfollow: (userId: string) => void
+  follow: (userId: string) => void
+  toggleIsFollowing: (isFetching: boolean, userID: string) => void
+  followingInProgress: string[]
 }
 
 export const Users: React.FC<UsersPropsType> = (
-    {
-        totalUsersCount,
-        pageSize,
-        currentPage,
-        onPageChanged,
-        followingInProgress,
-        follow,
-        unfollow, ...props
-    }) => {
+  {
+    totalUsersCount,
+    pageSize,
+    currentPage,
+    onPageChanged,
+    followingInProgress,
+    follow,
+    unfollow, ...props
+  }) => {
 
-    return (
-        <div>
-            {<Pagination totalUsersCount={totalUsersCount}
-                         pageSize={pageSize}
-                         currentPage={currentPage}
-                         onPageChanged={onPageChanged}/>}
-            {props.users.map(u =>
-                <User key={u.id}
-                      user={u}
-                      followingInProgress={followingInProgress}
-                      follow={follow}
-                      unfollow={unfollow}/>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      {<Pagination totalItemsCount={totalUsersCount}
+                   pageSize={pageSize}
+                   currentPage={currentPage}
+                   onPageChanged={onPageChanged}
+                   portionSize={10}/>}
+      {props.users.map(u =>
+        <User key={u.id}
+              user={u}
+              followingInProgress={followingInProgress}
+              follow={follow}
+              unfollow={unfollow}/>
+      )}
+    </div>
+  );
 };
--- src/Components/common/Pagination/Pagination.tsx
+++ src/Components/common/Pagination/Pagination.tsx
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./Pagination.module.css";
 
 type PaginationPropsType = {
-    totalUsersCount: number
-    pageSize: number
-    currentPage: number
-    onPageChanged: (currentPage: number) => void
+  totalItemsCount: number
+  pageSize: number
+  currentPage: number
+  onPageChanged: (currentPage: number) => void
+  portionSize: number
 }
 
 export const Pagination: React.FC<PaginationPropsType> = (
-    {
-        totalUsersCount,
-        pageSize,
-        onPageChanged,
-        currentPage
-    }
+  {
+    totalItemsCount,
+    pageSize,
+    onPageChanged,
+    currentPage,
+    portionSize
+  }
 ) => {
 
-    const pagesCount = Math.ceil(totalUsersCount / pageSize);
+  const pagesCount = Math.ceil(totalItemsCount / pageSize);
 
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i)
-    }
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i)
+  }
 
+  let portionCount = Math.ceil(pagesCount / portionSize);
+  let [portionNumber, setPortionNumber] = useState(1);
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  let rightPortionPageNumber = portionNumber * portionSize;
 
-    return (
-        <div className={s.pagination}>
-            {pages.map(el => {
-                return <span key={el}
-                             className={el === currentPage ? s.selectedPage : ''}
-                             onClick={() => onPageChanged(el)}>
+  return (
+    <div className={s.pagination}>
+      {portionNumber > 1 &&
+          <button onClick={() => {
+            setPortionNumber(portionNumber - 1)
+          }}>prev</button>}
+
+      {pages
+        .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+        .map(el => {
+        return <span key={el}
+                     className={el === currentPage ? s.selectedPage : s.page}
+                     onClick={() => onPageChanged(el)}>
                             {el}</span>
-            })}
-        </div>
-    );
+      })}
+
+      {portionCount > portionNumber &&
+          <button onClick={() => {
+            setPortionNumber(portionNumber + 1)
+          }}>next</button>}
+    </div>
+  );
 };
