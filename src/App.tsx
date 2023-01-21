239a1fe3c5c9099ac9a7b7435be83b4d452291e7
Сергей <privet15.89@mail.ru>
2023-01-21 17:49:36 +0500

    fix: BrowserRouter problem

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ const AppContainer = compose<ComponentType>(
 
 const MainApp = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <AppContainer/>
             </Provider>
