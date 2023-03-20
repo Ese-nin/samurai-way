bab623e2eac7712637e9effe35ce490d88ace642
Сергей <privet15.89@mail.ru>
2023-03-20 23:41:23 +0500

    feat: ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import React, {lazy, Suspense, useEffect} from 'react';
 import 'App.css';
-import {Navbar} from "Components/Navbar/Navbar";
-import {BrowserRouter, Route, withRouter} from "react-router-dom";
-import Music from "Components/Music/Music";
-import News from 'Components/News/News';
-import Settings from "Components/Settings/Settings";
-import {LoginPage} from "Components/Login/Login";
+import {BrowserRouter, Link, Redirect, Route, Switch, withRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store, useAppDispatch, useAppSelector} from "Redux/store";
 import {Preloader} from "Components/common/Preloader/Preloader";
-import {Header} from "Components/Header/Header";
+// import {Header} from "Components/Header/Header";
 import {getInitializedSuccess} from "Redux/selectors/app-selectors";
 import {initializeAppTC} from "Redux/Reducers/app-reducer";
+import Music from "./Components/Music/Music";
+import News from "./Components/News/News";
+import Settings from "./Components/Settings/Settings";
+import {LoginPage} from "./Components/Login/Login";
+
+import {Breadcrumb, Layout, Menu} from 'antd';
+import {LaptopOutlined, NotificationOutlined, UserOutlined} from '@ant-design/icons';
+import 'antd/dist/reset.css';
+import {Header} from "./Components/Header/Header";
+
+const {SubMenu} = Menu
+const {Content, Footer, Sider} = Layout;
 
 const Dialogs = lazy(() => import('Components/Dialogs/Dialogs'));
 const UsersPage = lazy(() => import('Components/Users/UsersContainer'));
@@ -27,11 +34,11 @@ const App: React.FC = () => {
         alert('Some error occured')
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(initializeAppTC())
         window.addEventListener('unhandledrejection', catchAllUnhandledErrors)
 
-        return ()=>{
+        return () => {
             window.removeEventListener('unhandledrejection', catchAllUnhandledErrors)
         }
     }, [])
@@ -41,28 +48,99 @@ const App: React.FC = () => {
     }
 
     return (
-        <div className={'app-wrapper'}>
+
+        <Layout>
             <Header/>
-            <Navbar/>
-            <div className={"app-wrapper-content"}>
-                <Suspense fallback={<Preloader/>}>
-                    <Route path='/profile/:userId?'
-                           render={() => <ProfileContainer/>}/>
-                    <Route path='/dialogs'
-                           render={() => <Dialogs/>}/>
-                    <Route path='/users'
-                           render={() => <UsersPage/>}/>
-                    <Route path='/music'
-                           render={() => <Music/>}/>
-                    <Route path='/news'
-                           render={() => <News/>}/>
-                    <Route path='/settings'
-                           render={() => <Settings/>}/>
-                    <Route path='/login'
-                           render={() => <LoginPage/>}/>
-                </Suspense>
-            </div>
-        </div>
+            <Content style={{padding: '0 50px'}}>
+                <Breadcrumb style={{margin: '16px 0'}}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <Layout className="site-layout-background" style={{padding: '24px 0'}}>
+                    <Sider className="site-layout-background" width={200}>
+                        <Menu
+                            mode="inline"
+                            defaultSelectedKeys={['1']}
+                            style={{height: '100%'}}
+                        >
+                            <SubMenu key="sub1" icon={<UserOutlined/>} title="My profile">
+                                <Menu.Item key="1"><Link to='/profile'>Profile</Link></Menu.Item>
+                                <Menu.Item key="2"><Link to='/dialogs'>Messages</Link></Menu.Item>
+                                {/*<Menu.Item key="3">option 3</Menu.Item>*/}
+                                {/*<Menu.Item key="4">option 4</Menu.Item>*/}
+                            </SubMenu>
+                            <SubMenu key="sub2" icon={<LaptopOutlined/>} title="Users">
+                                <Menu.Item key="5"><Link to='/users'>Users</Link></Menu.Item>
+                                {/*<Menu.Item key="6">option 6</Menu.Item>*/}
+                                {/*<Menu.Item key="7">option 7</Menu.Item>*/}
+                                {/*<Menu.Item key="8">option 8</Menu.Item>*/}
+                            </SubMenu>
+                            <SubMenu key="sub3" icon={<NotificationOutlined/>} title="Other">
+                                <Menu.Item key="9"><Link to='/music'>Music</Link></Menu.Item>
+                                <Menu.Item key="10"><Link to='/news'>News</Link></Menu.Item>
+                                <Menu.Item key="11"><Link to='/settings'>Settings</Link></Menu.Item>
+                                {/*<Menu.Item key="12">option 12</Menu.Item>*/}
+                            </SubMenu>
+                        </Menu>
+                    </Sider>
+                    <Content style={{padding: '0 24px', minHeight: 280}}>
+                        <Suspense fallback={<Preloader/>}>
+                            <Switch>
+                                <Route exact path='/'
+                                       render={() => <Redirect to={'/profile'}/>}/>
+                                <Route path='/profile/:userId?'
+                                       render={() => <ProfileContainer/>}/>
+                                <Route path='/dialogs'
+                                       render={() => <Dialogs/>}/>
+                                <Route path='/users'
+                                       render={() => <UsersPage/>}/>
+                                <Route path='/music'
+                                       render={() => <Music/>}/>
+                                <Route path='/news'
+                                       render={() => <News/>}/>
+                                <Route path='/settings'
+                                       render={() => <Settings/>}/>
+                                <Route path='/login'
+                                       render={() => <LoginPage/>}/>
+                                <Route path='*'
+                                       render={() => <div>404 PAGE NOT FOUND</div>}/>
+                            </Switch>
+                        </Suspense>
+                    </Content>
+                </Layout>
+            </Content>
+            <Footer style={{textAlign: 'center'}}>Samurai-Way ©2023</Footer>
+        </Layout>
+
+        // <div className={'app-wrapper'}>
+        //     <Header/>
+        //     <Navbar/>
+        //     <div className={"app-wrapper-content"}>
+        //         <Suspense fallback={<Preloader/>}>
+        //             <Switch>
+        //                 <Route exact path='/'
+        //                        render={() => <Redirect to={'/profile'}/>}/>
+        //                 <Route path='/profile/:userId?'
+        //                        render={() => <ProfileContainer/>}/>
+        //                 <Route path='/dialogs'
+        //                        render={() => <Dialogs/>}/>
+        //                 <Route path='/users'
+        //                        render={() => <UsersPage/>}/>
+        //                 <Route path='/music'
+        //                        render={() => <Music/>}/>
+        //                 <Route path='/news'
+        //                        render={() => <News/>}/>
+        //                 <Route path='/settings'
+        //                        render={() => <Settings/>}/>
+        //                 <Route path='/login'
+        //                        render={() => <LoginPage/>}/>
+        //                 <Route path='*'
+        //                        render={() => <div>404 PAGE NOT FOUND</div>}/>
+        //             </Switch>
+        //         </Suspense>
+        //     </div>
+        // </div>
     );
 }
 
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,32 +1,50 @@
 import React from "react";
-import s from "./Header.module.css"
-import {NavLink} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "Redux/store";
 import {logOut} from "Redux/Reducers/auth-reducer";
 import {getIsAuth, getLogin} from "Redux/selectors/auth-selectors";
+import {Avatar, Button, Col, Layout, Row} from "antd";
+import {UserOutlined} from "@ant-design/icons";
+import {Link} from "react-router-dom";
 
 
 export const Header: React.FC = () => {
+
+    const {Header} = Layout
 
     const isAuth = useAppSelector(getIsAuth)
     const login = useAppSelector(getLogin)
 
     const dispatch = useAppDispatch()
 
-    const logout = () => {
+    const logoutHandler = () => {
         dispatch(logOut())
     }
 
-    return <div className={s.header}>
-        <img
-            src={'https://img.freepik.com/premium-vector/star-logo_713663-134.jpg?w=826'}
-            alt={''}/>
+    return (
+        <Header className="header">
+            <Row>
+                <Col span={18}>
+                    <img
+                        src={'https://img.freepik.com/premium-vector/star-logo_713663-134.jpg?w=826'}
+                        alt={''} width={"50px"}/>
+                </Col>
 
-        <div className={s.loginBlock}>
-            {isAuth
-                ? <div>{login} <button onClick={logout}>LogOut</button></div>
-                : <NavLink to={'/login'}>Login</NavLink>}
-        </div>
-
-    </div>
+                {isAuth
+                    ? <>
+                        <Col span={2}>
+                            <Avatar style={{backgroundColor: '#87d068'}} icon={<UserOutlined/>} alt={login!}/>
+                        </Col>
+                        <Col span={4}>
+                            <Button onClick={logoutHandler}>LogOut</Button>
+                        </Col>
+                    </>
+                    :
+                    <Col span={6}>
+                        <Button>
+                            <Link to={'/login'}>Login</Link>
+                        </Button>
+                    </Col>}
+            </Row>
+        </Header>
+    )
 }
